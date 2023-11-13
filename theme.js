aa0bdfb3338811481190f453f2beafaa444df43c
milagro2 <morenoseijs06@gmail.com>
2023-11-13 12:00:52 +0100

    jss

--- theme.js
+++ theme.js
@@ -1,23 +1,48 @@
-function setTheme(theme) {
-    const themeLink = document.getElementById('theme-stylesheet');
-    switch (theme) {
-        case 'Dark':
-            themeLink.href = 'dark.css';
-            break;
-        case 'Neon':
-            themeLink.href = 'neon.css';
-            break;
-        case 'Hack':
-            themeLink.href = 'hack.css';
-            break;
-        default:
-            themeLink.href = 'light.css';
-    }
-    localStorage.setItem('theme', theme);
-    document.documentElement.className = theme;
-}
 
-(function () {
-    const theme = localStorage.getItem('theme') || 'Light';
-    setTheme(theme);
-})();
+    function setTheme(theme) {
+        const themeLink = document.getElementById('theme-stylesheet');
+        switch (theme) {
+            case 'Dark':
+                themeLink.href = 'dark.css';
+                break;
+            case 'Neon':
+                themeLink.href = 'neon.css';
+                break;
+            case 'Hack':
+                themeLink.href = 'hack.css';
+                break;
+            default:
+                themeLink.href = 'light.css';
+        }
+        localStorage.setItem('theme', theme);
+        document.documentElement.className = theme;
+    }
+
+    function setActiveLink() {
+        const links = document.querySelectorAll('#navbar ul li a');
+        const currentFileName = location.pathname.split('/').slice(-1)[0];
+
+        links.forEach(link => {
+            const linkFileName = link.getAttribute('href').split('/').slice(-1)[0];
+            if (currentFileName === linkFileName) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    }
+
+    (function () {
+        const theme = localStorage.getItem('theme') || 'Light';
+        setTheme(theme);
+        setActiveLink();
+    })();
+
+    const theme = localStorage.getItem('theme');
+
+    if (theme === 'Hack') {
+        const script = document.createElement('script');
+        script.src = 'hack.js';
+        document.body.appendChild(script);
+    }
+
