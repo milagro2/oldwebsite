f0e09d79d9b6e146bee8cec7cf2cfee2730c9720
milagro2 <morenoseijs06@gmail.com>
2023-11-13 12:05:26 +0100

    howi

--- theme.js
+++ theme.js
@@ -1,48 +1,45 @@
+function setTheme(theme) {
+    const themeLink = document.getElementById('theme-stylesheet');
+    switch (theme) {
+        case 'Dark':
+            themeLink.href = 'dark.css';
+            break;
+        case 'Neon':
+            themeLink.href = 'neon.css';
+            break;
+        case 'Hack':
+            themeLink.href = 'hack.css';
+            break;
+        default:
+            themeLink.href = 'light.css';
+    }
+    localStorage.setItem('theme', theme);
+    document.documentElement.className = theme;
+}
+function setActiveLink() {
+    const links = document.querySelectorAll('#navbar ul li a');
+    const currentFileName = window.location.pathname.split('/').pop(); // Get the last part of the path
 
-    function setTheme(theme) {
-        const themeLink = document.getElementById('theme-stylesheet');
-        switch (theme) {
-            case 'Dark':
-                themeLink.href = 'dark.css';
-                break;
-            case 'Neon':
-                themeLink.href = 'neon.css';
-                break;
-            case 'Hack':
-                themeLink.href = 'hack.css';
-                break;
-            default:
-                themeLink.href = 'light.css';
+    links.forEach(link => {
+        const linkFileName = link.getAttribute('href').split('/').pop(); // Get the last part of the link's path
+        if (currentFileName === linkFileName) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
         }
-        localStorage.setItem('theme', theme);
-        document.documentElement.className = theme;
-    }
+    });
+}
 
-    function setActiveLink() {
-        const links = document.querySelectorAll('#navbar ul li a');
-        const currentFileName = location.pathname.split('/').slice(-1)[0];
+(function () {
+    const theme = localStorage.getItem('theme') || 'Light';
+    setTheme(theme);
+    setActiveLink();
+})();
 
-        links.forEach(link => {
-            const linkFileName = link.getAttribute('href').split('/').slice(-1)[0];
-            if (currentFileName === linkFileName) {
-                link.classList.add('active');
-            } else {
-                link.classList.remove('active');
-            }
-        });
-    }
+const theme = localStorage.getItem('theme');
 
-    (function () {
-        const theme = localStorage.getItem('theme') || 'Light';
-        setTheme(theme);
-        setActiveLink();
-    })();
-
-    const theme = localStorage.getItem('theme');
-
-    if (theme === 'Hack') {
-        const script = document.createElement('script');
-        script.src = 'hack.js';
-        document.body.appendChild(script);
-    }
-
+if (theme === 'Hack') {
+    const script = document.createElement('script');
+    script.src = 'hack.js';
+    document.body.appendChild(script);
+}
