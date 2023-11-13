170869d58529f96963cd1fb04747cf7c7acb96da
milagro2 <morenoseijs06@gmail.com>
2023-11-13 14:07:01 +0100

    ee

--- theme.js
+++ theme.js
@@ -1,23 +1,27 @@
 function setTheme(theme) {
     const themeLink = document.getElementById('theme-stylesheet');
-    const darkButton = document.getElementById('dark');
+    const themeButtons = document.querySelectorAll('.theme-button');
+
+    themeButtons.forEach(button => {
+        button.classList.remove('light-theme', 'dark-theme', 'neon-theme', 'hack-theme');
+    });
 
     switch (theme) {
         case 'Dark':
             themeLink.href = 'dark.css';
-            darkButton.classList.add('active'); 
+            themeButtons.forEach(button => button.classList.add('dark-theme'));
             break;
         case 'Neon':
             themeLink.href = 'neon.css';
-            darkButton.classList.remove('active');
+            themeButtons.forEach(button => button.classList.add('neon-theme'));
             break;
         case 'Hack':
             themeLink.href = 'hack.css';
-            darkButton.classList.remove('active');
+            themeButtons.forEach(button => button.classList.add('hack-theme'));
             break;
         default:
             themeLink.href = 'light.css';
-            darkButton.classList.remove('active'); 
+            themeButtons.forEach(button => button.classList.add('light-theme'));
     }
 
     localStorage.setItem('theme', theme);
