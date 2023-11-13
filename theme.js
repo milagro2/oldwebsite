725fef327a09f439fcc2e5b226622cc4b99b33ef
milagro2 <morenoseijs06@gmail.com>
2023-11-13 14:14:22 +0100

    heyy

--- theme.js
+++ theme.js
@@ -1,27 +1,38 @@
 function setTheme(theme) {
     const themeLink = document.getElementById('theme-stylesheet');
-    const themeButtons = document.querySelectorAll('.theme-button');
-
-    themeButtons.forEach(button => {
-        button.classList.remove('light-theme', 'dark-theme', 'neon-theme', 'hack-theme');
-    });
+    const darkButton = document.getElementById('dark');
+    const lightButton = document.getElementById('light');
+    const neonButton = document.getElementById('neon');
+    const hackButton = document.getElementById('hack');
 
     switch (theme) {
         case 'Dark':
             themeLink.href = 'dark.css';
-            themeButtons.forEach(button => button.classList.add('dark-theme'));
+            darkButton.classList.add('active');
+            lightButton.classList.remove('active');
+            neonButton.classList.remove('active');
+            hackButton.classList.remove('active');
             break;
         case 'Neon':
             themeLink.href = 'neon.css';
-            themeButtons.forEach(button => button.classList.add('neon-theme'));
+            darkButton.classList.remove('active');
+            neonButton.classList.add('active');
+            lightButton.classList.remove('active');
+            hackButton.classList.remove('active');
             break;
         case 'Hack':
             themeLink.href = 'hack.css';
-            themeButtons.forEach(button => button.classList.add('hack-theme'));
+            darkButton.classList.remove('active');
+            neonButton.classList.remove('active');
+            lightButton.classList.remove('active');
+            hackButton.classList.add('active');
             break;
         default:
             themeLink.href = 'light.css';
-            themeButtons.forEach(button => button.classList.add('light-theme'));
+            darkButton.classList.remove('active');
+            neonButton.classList.remove('active');
+            lightButton.classList.add('active');
+            hackButton.classList.remove('active');
     }
 
     localStorage.setItem('theme', theme);
