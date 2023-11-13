2f5f2d47fc85a80d78fab8390a8c4f39f7acc015
milagro2 <morenoseijs06@gmail.com>
2023-11-13 14:01:20 +0100

    mayb

--- theme.js
+++ theme.js
@@ -1,22 +1,28 @@
 function setTheme(theme) {
     const themeLink = document.getElementById('theme-stylesheet');
+    const darkButton = document.getElementById('dark');
+
     switch (theme) {
         case 'Dark':
             themeLink.href = 'dark.css';
+            darkButton.classList.add('active'); 
             break;
         case 'Neon':
             themeLink.href = 'neon.css';
+            darkButton.classList.remove('active');
             break;
         case 'Hack':
             themeLink.href = 'hack.css';
+            darkButton.classList.remove('active');
             break;
         default:
             themeLink.href = 'light.css';
+            darkButton.classList.remove('active'); 
     }
+
     localStorage.setItem('theme', theme);
     document.documentElement.className = theme;
 }
-
 
 (function () {
     const theme = localStorage.getItem('theme') || 'Light';
